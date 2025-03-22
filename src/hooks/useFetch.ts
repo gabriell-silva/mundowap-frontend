a0ea945752e48f8a486fb49856aca5c21ea4b127
gabriell-silva <gabrielsilva.freires2@gmail.com>
2025-03-22 18:23:46 -0300

    CHORE: hook for request

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,37 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import React, { useCallback } from 'react';
+
+export default function useFetch<T, D = unknown>() {
+    const [loading, setLoading] = React.useState<boolean>(false);
+    const [error, setError] = React.useState<Error | null>(null);
+
+    const request = async ({
+      url,
+      method = "get",
+      ...options
+    }: AxiosRequestConfig) => {
+      setLoading(true);
+      setError(null);
+
+      try {
+          const {data}: AxiosResponse = await axios({
+              url,
+              method,
+              ...options
+          });
+
+          setLoading(false);
+          return data;
+      } catch (error) {
+          setError(error instanceof Error ? error : new Error(String(error)));
+          setLoading(false);
+          throw error;
+      }
+    }
+
+    return [
+        request,
+        loading,
+        error,
+    ] as const;
+}
