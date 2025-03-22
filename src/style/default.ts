824bd874df8e029412a226929a04d4acf96d5178
gabriell-silva <gabrielsilva.freires2@gmail.com>
2025-03-22 18:25:21 -0300

    CHORE: styles generic

---
+++ src/style/default.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+const FlexColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const FlexRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+`;
+
+const Box = styled.div`
+    width: "100%",
+`;
+
+export {
+  FlexColumn,
+  FlexRow,
+  Box
+}
